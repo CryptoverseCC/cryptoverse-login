e0fb43f563ef91a77c6c28c620d1e2c0c3823850
Grzegorz Kapkowski <grzegorz.kapkowski@gmail.com>
2022-08-02 20:58:31 +0200

    Maybe fix demo app

--- tests/cryptoverse-login-demo/demo/src/run.sh
+++ tests/cryptoverse-login-demo/demo/src/run.sh
@@ -3,4 +3,4 @@ set -e
 python manage.py migrate
 echo "Running server on PORT $PORT"
 #python manage.py runserver 0.0.0.0:$PORT
-gunicorn -b 0.0.0.0:$PORT -w 2 manager.wsgi
+gunicorn -b 0.0.0.0:$PORT -w 1 manager.wsgi
