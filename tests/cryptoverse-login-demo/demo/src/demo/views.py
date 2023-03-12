cedb255b0558c44c454e926aadda63237eda1fa2
Grzegorz Kapkowski <grzegorz.kapkowski@gmail.com>
2023-03-12 17:23:19 +0100

    Show ID token in Demo App

--- tests/cryptoverse-login-demo/demo/src/demo/views.py
+++ tests/cryptoverse-login-demo/demo/src/demo/views.py
@@ -1,9 +1,9 @@
 import logging
 import json
-from jwt import JWT, jwk, jws
+import base64
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.conf import settings
 
 logger = logging.getLogger("django")
 
@@ -16,7 +16,10 @@ def home(request):
 def protected(request):
     id_token = request.session.get("oidc_id_token")
     if id_token:
-        id_token = jws.JWS().decode(id_token, do_verify=False)
+        _, payload, _ = id_token.split(".")
+        padded = payload + "=" * (4 - len(payload) % 4)
+        decoded = base64.b64decode(padded)
+        return json.loads(decoded)
     return render(
         request, "demo/protected.html", {"user": request.user, "data": id_token}
     )
