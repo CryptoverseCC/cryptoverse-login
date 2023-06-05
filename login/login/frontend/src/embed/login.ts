c6af80962707265e72e32cd38e7a399de7315ca0
Grzegorz Kapkowski <grzegorz.kapkowski@gmail.com>
2023-06-05 21:31:28 +0200

    Checkpoint: Working Login!!!

--- login/login/frontend/src/embed/login.ts
+++ login/login/frontend/src/embed/login.ts
@@ -1,65 +1,72 @@
 import {
-  TInnerLoginRequest, TLoginResponse,
+    TInnerLoginRequest, TLoginResponse,
 } from "../services/loginProvider";
 import { signers } from "./signers";
 import { Window } from "./utils";
 
 
 export const login = async ({ address, csrf, challenge }: TInnerLoginRequest): Promise<TLoginResponse> => {
-  const getInjectedProvider = (await import(/* webpackPrefetch: true */ "web3modal")).getInjectedProvider;
+    const getInjectedProvider = (await import(/* webpackPrefetch: true */ "web3modal")).getInjectedProvider;
 
-  const sentry = (window as unknown as Window).Sentry;
-  let message = `Cryptoverse Login - auth.cryptoverse.cc|${csrf}|${challenge}`;
-  let signer;
+    const sentry = (window as unknown as Window).Sentry;
+    let message = `Cryptoverse Login - auth.cryptoverse.cc|${csrf}|${challenge}`;
+    let signer;
 
-  const pp = getInjectedProvider();
+    const pp = getInjectedProvider();
 
-  sentry.onLoad(() => sentry?.setContext("provider", {
-    name: pp?.name,
-    id: pp?.id,
-  }));
+    sentry.onLoad(() => sentry?.setContext("provider", {
+        name: pp?.name,
+        id: pp?.id,
+    }));
 
-  sentry.onLoad(() => sentry?.setContext("user", {
-    address
-  }));
+    sentry.onLoad(() => sentry?.setContext("user", {
+        address
+    }));
 
-  if (pp) {
-    const name = pp.name.toLowerCase();
-    signer = signers[name];
-  }
+    if (pp) {
+        console.log(pp);
+        console.log(signers);
+        const name = pp.name.toLowerCase();
+        console.log(name);
+        signer = signers[name];
+        console.log(signer);
+    } else {
+        console.warn("No named signeer")
+    }
 
-  if (!signer) {
-    signer = signers.default;
-  }
+    if (!signer) {
+        signer = signers.default;
+    }
 
-  signer = signer();
+    signer = signer();
 
-  let signature;
+    let signature;
 
-  try {
-    signature = await signer.sign(message)
-  } catch (error) {
-    sentry?.captureException(error); // TODO: wrap Sentry in some service eg. ErrorReporter
-    return null;
-  }
+    try {
+        signature = await signer.sign(message, address)
+    } catch (error) {
+        console.error(error);
+        sentry?.captureException(error); // TODO: wrap Sentry in some service eg. ErrorReporter
+        return null;
+    }
 
 
-  try {
-    await signer.verify(signature, message, address);
-  } catch (error) {
-    sentry?.captureException(error); // TODO: wrap Sentry in some service eg. ErrorReporter
-    return null;
-  }
+    try {
+        await signer.verify(signature, message, address);
+    } catch (error) {
+        sentry?.captureException(error); // TODO: wrap Sentry in some service eg. ErrorReporter
+        return null;
+    }
 
-  try {
-    return {
-      signature,
-      csrf,
-      challenge,
-      address,
-    };
-  } catch (error) {
-    sentry?.captureException(error); // TODO: wrap Sentry in some service eg. ErrorReporter
-    return null;
-  }
+    try {
+        return {
+            signature,
+            csrf,
+            challenge,
+            address,
+        };
+    } catch (error) {
+        sentry?.captureException(error); // TODO: wrap Sentry in some service eg. ErrorReporter
+        return null;
+    }
 };
