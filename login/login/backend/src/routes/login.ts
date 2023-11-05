b73ea962a25bf3a3b4f463d7d56cd777f2705131
Grzegorz Kapkowski <grzegorz.kapkowski@gmail.com>
2023-11-05 15:09:37 +0100

    Use metadata to display origin website name

--- login/login/backend/src/routes/login.ts
+++ login/login/backend/src/routes/login.ts
@@ -11,9 +11,9 @@ import { checkRestrictions, getRestrictions, TCheckResults } from "./restriction
 
 // native prom-client metric (no prefix)
 const loginStatus = new promClient.Counter({
-  name: 'login_status',
-  help: 'Cryptoverse Login Status',
-  labelNames: ['status', 'client']
+    name: 'login_status',
+    help: 'Cryptoverse Login Status',
+    labelNames: ['status', 'client']
 });
 
 // @ts-ignore
@@ -25,180 +25,182 @@ export const router = Router();
 const csrfProtection = csrf({ cookie: true });
 
 router.get("/", csrfProtection, async function (req, res, next) {
-  // Parses the URL query
-  const query = (req.query as unknown) as {
-    login_challenge: string;
-  };
+    // Parses the URL query
+    const query = (req.query as unknown) as {
+        login_challenge: string;
+    };
 
-  loginStatus.labels('login-init', 'unknown').inc(1);
+    loginStatus.labels('login-init', 'unknown').inc(1);
 
-  // The challenge is used to fetch information about the login request from ORY Hydra.
-  const challenge = query.login_challenge;
+    // The challenge is used to fetch information about the login request from ORY Hydra.
+    const challenge = query.login_challenge;
 
-  if (!challenge) {
-    res.status(400).send('Missing Param');
-    loginStatus.labels('error-bad-request-login-init', 'unknown').inc(1);
-    return;
-  }
+    if (!challenge) {
+        res.status(400).send('Missing Param');
+        loginStatus.labels('error-bad-request-login-init', 'unknown').inc(1);
+        return;
+    }
 
 
-  let hydraRequest: AxiosResponse<OAuth2LoginRequest>;
+    let hydraRequest: AxiosResponse<OAuth2LoginRequest>;
 
-  try {
-      hydraRequest = await hydra.getOAuth2LoginRequest({ loginChallenge: challenge })
-  } catch (error) {
-    console.error(error);
-    loginStatus.labels('error-during-init-error', 'unknown').inc(1);
-    next(error);
-    return;
-  }
+    try {
+        hydraRequest = await hydra.getOAuth2LoginRequest({ loginChallenge: challenge })
+    } catch (error) {
+        console.error(error);
+        loginStatus.labels('error-during-init-error', 'unknown').inc(1);
+        next(error);
+        return;
+    }
 
-  const clientId = hydraRequest.data.client.client_id;
+    const clientId = hydraRequest.data.client.client_id;
+    const metadata = hydraRequest.data.client.metadata;
 
-  const restrictions = await getRestrictions(hydraRequest.data.client, next);
+    const restrictions = await getRestrictions(hydraRequest.data.client, next);
 
-  const renderData = {
-    sourceId: "default",
-    clientId: clientId,
-    csrfToken: req.csrfToken(),
-    challenge: challenge,
-    restrictions: JSON.stringify(restrictions),
-    layout: false
-  };
+    const renderData = {
+        sourceId: "default",
+        clientId: clientId,
+        csrfToken: req.csrfToken(),
+        challenge: challenge,
+        restrictions: JSON.stringify(restrictions),
+        metadata: JSON.stringify(metadata),
+        layout: false
+    };
 
-  const initialURL: string = hydraRequest.data.request_url;
+    const initialURL: string = hydraRequest.data.request_url;
 
-  if (initialURL) {
-    const url = new URL(initialURL);
-    renderData.sourceId = url.searchParams.get("ac") || renderData.sourceId;
-  }
+    if (initialURL) {
+        const url = new URL(initialURL);
+        renderData.sourceId = url.searchParams.get("ac") || renderData.sourceId;
+    }
 
-  loginStatus.labels('login-init-response', clientId).inc(1);
+    loginStatus.labels('login-init-response', clientId).inc(1);
 
-  res.render("index.html", renderData);
+    res.render("index.html", renderData);
 });
 
 router.post("/process", csrfProtection, async function (req, res, next) {
-  // The challenge is now a hidden input field, so let's take it from the request body instead
-  const loginData = req.body; // POST
-  //const loginData = req.query; // GET
+    // The challenge is now a hidden input field, so let's take it from the request body instead
+    const loginData = req.body; // POST
+    //const loginData = req.query; // GET
 
-  const challenge = loginData.challenge;
+    const challenge = loginData.challenge;
 
-  if (!challenge) {
-    res.status(400).send('Missing Param');
-    loginStatus.labels('error-missing-challenge', 'unknown').inc(1);
-    return;
-  }
+    if (!challenge) {
+        res.status(400).send('Missing Param');
+        loginStatus.labels('error-missing-challenge', 'unknown').inc(1);
+        return;
+    }
 
-  let hydraRequest;
+    let hydraRequest;
 
-  try {
-      hydraRequest = await hydra.getOAuth2LoginRequest({
-          loginChallenge: challenge,
-      });
-  } catch (error) {
-    console.log("Hydra error")
-    console.error(error)
-    loginStatus.labels('error-retrieving-hydra-request', 'unknown').inc(1);
-    next(error);
-    return;
-  }
-
-  const clientId = hydraRequest.data.client.client_id;
-
-  loginStatus.labels('login-start', clientId).inc(1);
-
-  const data = {
-    address: loginData.address,
-    msg: `Cryptoverse Login - auth.cryptoverse.cc|${loginData._csrf}|${loginData.challenge}`,
-    sig: loginData.signature
-  };
-
-  const digest = ethers.getBytes(
-    ethers.hashMessage(data.msg)
-  );
-  let recoveredAddress = await ethers.recoverAddress(
-    digest,
-    data.sig
-  );
-
-  recoveredAddress = recoveredAddress.toLowerCase();
-
-  console.log("Recovered Address: ", recoveredAddress, typeof recoveredAddress);
-
-  if (recoveredAddress != loginData.address.toLowerCase()) {
-    // Looks like the user provided invalid credentials, let's show the ui again...
-
-    res.render("error.html", {
-      csrfToken: req.csrfToken(),
-
-      challenge: challenge,
-
-      error: "Login error: address mismatch."
-    });
-    loginStatus.labels('error-address-mismatch', clientId).inc(1);
-    return;
-  }
-
-  const allow = (clientId: string) => {
-    // Seems like the user authenticated! Let's tell hydra...
-    hydra
-        .acceptOAuth2LoginRequest({
+    try {
+        hydraRequest = await hydra.getOAuth2LoginRequest({
             loginChallenge: challenge,
-            acceptOAuth2LoginRequest: {
-                subject: `${recoveredAddress}`
-            }
-        })
-      .then(function (response) {
-        // All we need to do now is to redirect the user back to hydra!
-        console.info(
-          `LOGIN ${new Date().toISOString()} ${clientId} ${recoveredAddress}`
-        );
-        if (loginData.response == "api") {
-          res.send({ redirect: response.data.redirect_to });
-        } else {
-          res.redirect(response.data.redirect_to);
-        }
-        loginStatus.labels('success', clientId).inc(1);
-      })
-      // This will handle any error that happens when making HTTP calls to hydra
-      .catch(function (error) {
+        });
+    } catch (error) {
+        console.log("Hydra error")
+        console.error(error)
+        loginStatus.labels('error-retrieving-hydra-request', 'unknown').inc(1);
+        next(error);
+        return;
+    }
+
+    const clientId = hydraRequest.data.client.client_id;
+
+    loginStatus.labels('login-start', clientId).inc(1);
+
+    const data = {
+        address: loginData.address,
+        msg: `Cryptoverse Login - auth.cryptoverse.cc|${loginData._csrf}|${loginData.challenge}`,
+        sig: loginData.signature
+    };
+
+    const digest = ethers.getBytes(
+        ethers.hashMessage(data.msg)
+    );
+    let recoveredAddress = await ethers.recoverAddress(
+        digest,
+        data.sig
+    );
+
+    recoveredAddress = recoveredAddress.toLowerCase();
+
+    console.log("Recovered Address: ", recoveredAddress, typeof recoveredAddress);
+
+    if (recoveredAddress != loginData.address.toLowerCase()) {
+        // Looks like the user provided invalid credentials, let's show the ui again...
+
+        res.render("error.html", {
+            csrfToken: req.csrfToken(),
+
+            challenge: challenge,
+
+            error: "Login error: address mismatch."
+        });
+        loginStatus.labels('error-address-mismatch', clientId).inc(1);
+        return;
+    }
+
+    const allow = (clientId: string) => {
+        // Seems like the user authenticated! Let's tell hydra...
+        hydra
+            .acceptOAuth2LoginRequest({
+                loginChallenge: challenge,
+                acceptOAuth2LoginRequest: {
+                    subject: `${recoveredAddress}`
+                }
+            })
+            .then(function (response) {
+                // All we need to do now is to redirect the user back to hydra!
+                console.info(
+                    `LOGIN ${new Date().toISOString()} ${clientId} ${recoveredAddress}`
+                );
+                if (loginData.response == "api") {
+                    res.send({ redirect: response.data.redirect_to });
+                } else {
+                    res.redirect(response.data.redirect_to);
+                }
+                loginStatus.labels('success', clientId).inc(1);
+            })
+            // This will handle any error that happens when making HTTP calls to hydra
+            .catch(function (error) {
+                loginStatus.labels('error-unknown', clientId).inc(1);
+                next(error);
+            });
+    };
+
+    const clientRestrictions = await getRestrictions(
+        hydraRequest.data.client,
+        next
+    );
+
+    if (!clientRestrictions) {
+        // No Token restrictions flow, allow everyone.
+        allow(clientId);
+        return;
+    }
+
+    let allChecks: TCheckResults;
+
+    try {
+        allChecks = await checkRestrictions(clientRestrictions, recoveredAddress);
+    } catch (error) {
         loginStatus.labels('error-unknown', clientId).inc(1);
         next(error);
-      });
-  };
+        return;
+    }
 
-  const clientRestrictions = await getRestrictions(
-    hydraRequest.data.client,
-    next
-  );
-
-  if (!clientRestrictions) {
-    // No Token restrictions flow, allow everyone.
-    allow(clientId);
-    return;
-  }
-
-  let allChecks: TCheckResults;
-
-  try {
-    allChecks = await checkRestrictions(clientRestrictions, recoveredAddress);
-  } catch (error) {
-    loginStatus.labels('error-unknown', clientId).inc(1);
-    next(error);
-    return;
-  }
-
-  if (allChecks.every(check => check.result)) {
-    allow(clientId)
-  } else {
-    console.error(`Restriction checks failure: ${JSON.stringify(allChecks)}`)
-    loginStatus.labels('error-restricted', clientId).inc(1);
-    next(
-      new Error(
-        `Your address fails conditions: ${JSON.stringify(allChecks, null, 2)}`
-      )
-    );
-  }
+    if (allChecks.every(check => check.result)) {
+        allow(clientId)
+    } else {
+        console.error(`Restriction checks failure: ${JSON.stringify(allChecks)}`)
+        loginStatus.labels('error-restricted', clientId).inc(1);
+        next(
+            new Error(
+                `Your address fails conditions: ${JSON.stringify(allChecks, null, 2)}`
+            )
+        );
+    }
 });
